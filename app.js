6c8f1321a3670bef4ce1d08dbb2c4e5bb8093082
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-11 14:08:47 -0500

    Added dynamic temp humidity and wind speed from API

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 let apiKey = "38068f8dbeb00ea35c18384c9fb8712c";
-let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${apiKey}`;
+let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
 
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
   //going to use variable to try to set text content, but first, going to display city name with created p tag
-  // let cityname = $("h3#cityname").text();
-  let pTag = $("<p>");
-  let cityname = response.city.name;
-  pTag.text(cityname);
-  $("h3#cityname").append(pTag);
+  $("h3#cityname").text(response.city.name);
+  $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
+  $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
+  $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
 });
