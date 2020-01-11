35dae19c61d2e7b2347fdbaf39b223bc7f6c9852
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-11 15:46:00 -0500

    Successfully connected UV Index and put text content in corresponding p tag

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 let apiKey = "38068f8dbeb00ea35c18384c9fb8712c";
 let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
-
+/*
+http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
+*/
+let moscowLon = "37.6156";
+let moscowLat = "55.7522";
+let uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${moscowLat}&lon=${moscowLon}`;
 $.ajax({
   url: queryURL,
   method: "GET"
@@ -10,4 +15,11 @@ $.ajax({
   $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
   $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
   $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
+});
+
+$.ajax({
+  url: uvURL,
+  method: "GET"
+}).then(function(response) {
+  $("p#uvindex").text(`UV Index: ${response.value}`);
 });
