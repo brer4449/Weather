e87f8bc9ca400c0de99d1e98453bcfb351585fe4
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-11 16:33:31 -0500

    Have successful if else statement switching back and forth from Chicago to Moscow

--- JS/app.js
+++ JS/app.js
@@ -1,23 +1,45 @@
-let apiKey = "38068f8dbeb00ea35c18384c9fb8712c";
-let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
-let moscowLon = "37.6156";
-let moscowLat = "55.7522";
-let uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${moscowLat}&lon=${moscowLon}`;
+$("document").ready(function() {
+  let apiKey = "38068f8dbeb00ea35c18384c9fb8712c";
+  let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
+  let moscowLon = "37.6156";
+  let moscowLat = "55.7522";
+  let uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${moscowLat}&lon=${moscowLon}`;
+  let searchInput = $("input#searchinput");
+  let searchBtn = $("button#search");
+  searchBtn.on("click", function(e) {
+    e.preventDefault();
+    let citySearch = searchInput.val();
+    if (citySearch === "Chicago" || citySearch === "chicago") {
+      queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=Chicago,us&units=imperial&APPID=${apiKey}`;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        //Assigning text content of corresponding element tag from API
+        $("h3#cityname").text(response.city.name);
+        $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
+        $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
+        $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
+      });
+    } else {
+      queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+        //Assigning text content of corresponding element tag from API
+        $("h3#cityname").text(response.city.name);
+        $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
+        $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
+        $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
+      });
+    }
+  });
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  //Assigning text content of corresponding element tag from API
-  $("h3#cityname").text(response.city.name);
-  $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
-  $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
-  $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
-});
-
-$.ajax({
-  url: uvURL,
-  method: "GET"
-}).then(function(response) {
-  $("p#uvindex").text(`UV Index: ${response.value}`);
+  $.ajax({
+    url: uvURL,
+    method: "GET"
+  }).then(function(response) {
+    $("p#uvindex").text(`UV Index: ${response.value}`);
+  });
 });
