fe44e4223f399a86b0910f3e323ea43285f256b6
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-11 15:58:47 -0500

    Changed the city list items into buttons

--- JS/app.js
+++ JS/app.js
@@ -1,16 +1,14 @@
 let apiKey = "38068f8dbeb00ea35c18384c9fb8712c";
-let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
-/*
-http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
-*/
+// let queryURL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&units=imperial&APPID=${apiKey}`;
 let moscowLon = "37.6156";
 let moscowLat = "55.7522";
-let uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${moscowLat}&lon=${moscowLon}`;
+// let uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${moscowLat}&lon=${moscowLon}`;
+
 $.ajax({
   url: queryURL,
   method: "GET"
 }).then(function(response) {
-  //going to use variable to try to set text content, but first, going to display city name with created p tag
+  //Assigning text content of corresponding element tag from API
   $("h3#cityname").text(response.city.name);
   $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
   $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
