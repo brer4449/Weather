bed7f955d6df86f39e6e888cd7eab276f09d6b25
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-01-16 14:59:46 -0500

    Now only creates one button for each search button click and clears input field of search

--- assets/js/app.js
+++ assets/js/app.js
@@ -65,56 +65,58 @@ $("document").ready(function () {
   searchBtn.on("click", function (e) {
     e.preventDefault();
     let citySearch = searchInput.val();
+    searchInput.val("");
     let savedCities = JSON.parse(localStorage.getItem("savedCities"))
     if (!savedCities) {
       savedCities = [];
     }
     savedCities.push(citySearch);
-
+    $(".list-group").empty();
     localStorage.setItem("savedCities", JSON.stringify(savedCities));
     for (let i = 0; i < savedCities.length; i++) {
       let cityBtn = $("<button>");
-
       cityBtn.text(savedCities[i]);
       cityBtn.addClass("btn btn-primary");
       // $(`li.${i}`).remove(cityBtn);
-      $(`li.${i}`).append(cityBtn);
+      $(".list-group").append(cityBtn);
+
+      cityBtn.on("click", function (e) {
+        e.preventDefault();
+        queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${$(this).text()},us&units=imperial&APPID=${apiKey}`;
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function (response) {
+          $("h3#cityname").text(`${response.city.name}`);
+          $("img#icon").attr("src", `http://openweathermap.org/img/wn/${response.list[0].weather[0].icon}@2x.png`);
+          $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
+          $("h6#date").text(response.list[0].dt_txt)
+          $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
+          $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
+
+          let cityLat = response.city.coord.lat;
+          let cityLon = response.city.coord.lon;
+          uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${cityLat}&lon=${cityLon}`;
+          $.ajax({
+            url: uvURL,
+            method: "GET",
+          }).then(function (response) {
+            $("p#uvindex").text(`UV Index: ${response.value}`);
+          });
+
+          for (let i = 1; i < 6; i++) {
+            $(`span#${i}`).find($("img.icon")).attr("src", `http://openweathermap.org/img/wn/${response.list[8 * i - 1].weather[0].icon}@2x.png`);
+            $(`span#${i}`).find($("h3.date")).text(`${response.list[8 * i - 1].dt_txt}`)
+            $(`span#${i}`).find($("p.temp")).text(`Temp: ${response.list[8 * i - 1].main.temp}°F`);
+            $(`span#${i}`).find($("p.humidity")).text(`Humidity: ${response.list[8 * i - 1].main.humidity}%`);
+          }
+        });
+      });
     }
   });
   //On click event to fire off and get weather info of the city whose button was clicked
-  cityBtn.on("click", function (e) {
-    e.preventDefault();
-    queryURL = `http://api.openweathermap.org/data/2.5/forecast?q=${$(this).text()},us&units=imperial&APPID=${apiKey}`;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      $("h3#cityname").text(`${response.city.name}`);
-      $("img#icon").attr("src", `http://openweathermap.org/img/wn/${response.list[0].weather[0].icon}@2x.png`);
-      $("h5#temp").text(`Temperature: ${response.list[0].main.temp}°F`);
-      $("h6#date").text(response.list[0].dt_txt)
-      $("p#humidity").text(`Humidity: ${response.list[0].main.humidity}%`);
-      $("p#windspeed").text(`Wind: ${response.list[0].wind.speed} mph`);
-
-      let cityLat = response.city.coord.lat;
-      let cityLon = response.city.coord.lon;
-      uvURL = `http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${cityLat}&lon=${cityLon}`;
-      $.ajax({
-        url: uvURL,
-        method: "GET",
-      }).then(function (response) {
-        $("p#uvindex").text(`UV Index: ${response.value}`);
-      });
-
-      for (let i = 1; i < 6; i++) {
-        $(`span#${i}`).find($("img.icon")).attr("src", `http://openweathermap.org/img/wn/${response.list[8 * i - 1].weather[0].icon}@2x.png`);
-        $(`span#${i}`).find($("h3.date")).text(`${response.list[8 * i - 1].dt_txt}`)
-        $(`span#${i}`).find($("p.temp")).text(`Temp: ${response.list[8 * i - 1].main.temp}°F`);
-        $(`span#${i}`).find($("p.humidity")).text(`Humidity: ${response.list[8 * i - 1].main.humidity}%`);
-      }
-    });
-  });
 });
 
 
